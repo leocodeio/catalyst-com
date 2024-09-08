f10c48d4de8a0172eed9398df0f505f49cf2993b
leocodeio <basicleo@787>
2024-09-08 13:50:48 +0530

    updated for deploy

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -1,43 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { AiOutlineLogin } from "react-icons/ai";
-import { useLoaderData } from "@remix-run/react";
-import { UserData } from "../../model/user-data";
+import { Link } from "@remix-run/react";
+import { IoLogOut } from "react-icons/io5";
 
 const Header = () => {
-  const user = useLoaderData<UserData>();
-
-  const loginIconStyle: React.CSSProperties = {
-    color: "#ffed00",
-    backgroundColor: "black",
-    borderRadius: "50px",
-    padding: "3px",
-    width: "30px",
-    height: "30px",
-    transform: user ? "" : "scaleX(-1)",
-  };
 
   return (
     <div className="header-glass-effect fixed w-full flex items-center justify-between h-[75px] shadow-md z-50 px-8">
       {/* <h1>{user.id}</h1> */}
-      <div>
+      <Link to="/home">
         <img
           src="./images/logo.png"
           alt="Catalyst"
           className="h-[75px] w-[150px]"
         />
-      </div>
-      {!(user.id === "") ? (
-        <Link className="flex align-center gap-2" to="/login">
-          Login/Logout
-          <AiOutlineLogin style={loginIconStyle} />
-        </Link>
-      ) : (
-        <Link className="flex align-center gap-2" to="/login">
-          Login/Logout
-          <AiOutlineLogin style={loginIconStyle} />
-        </Link>
-      )}
+      </Link>
+      <Link className="flex align-center gap-2" to="/login">
+        <IoLogOut className="h-[30px] w-[30px]" style={{ color: "yellow" }} />
+      </Link>
     </div>
   );
 };
---
+++ app/components/homeComponents/Welcome.tsx
@@ -0,0 +1,21 @@
+import { useLoaderData } from "@remix-run/react";
+import { UserData } from "../../model/user-data";
+
+const Welcome = () => {
+  const user = useLoaderData<UserData>();
+  return (
+    <div className="bg-black bg-opacity-45 text-primary flex flex-col items-center justify-center text-center p-4 rounded-lg">
+      <h1 className="text-2xl mb-4">Welcome to Catalyst - {user.name}</h1>
+      <h2 className="mb-4">
+        Reach out to us at <br />
+        <a href="https://www.linkedin.com/in/sai-harsha-vardhan-pittada-8a9a74252/">
+          <p> {"Click -->"}</p>
+          <h3 className="text-blue">@leocodeio</h3>
+        </a>
+      </h2>
+      <p className="mt-4">Thank you for staying connected!</p>
+    </div>
+  );
+};
+
+export default Welcome;
